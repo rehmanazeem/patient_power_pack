63d91cccd2959b05ff1adf302f97ff136be21d7b
Azeem Ur Rehman <rehmanazeem915@gmail.com>
2024-10-25 16:32:52 +0200

    Added AWS model

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 from flask import Flask, render_template, request, jsonify
 import requests  # or use your LLM library directly
+from dotenv import load_dotenv
+import os
+
+from langchain_aws import ChatBedrock
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
+
+REGION_NAME = os.getenv("REGION_NAME")
+AWS_KEY = os.getenv("AWS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+AWS_SESSION_TOKEN= os.getenv("AWS_SESSION_TOKEN")
 
 app = Flask(__name__)
+try:
+    model = ChatBedrock(model_id="meta.llama3-1-8b-instruct-v1:0",
+                        region_name=REGION_NAME,
+                        aws_access_key_id=AWS_KEY,
+                        aws_secret_access_key=AWS_SECRET_KEY,
+                        aws_session_token=AWS_SESSION_TOKEN)
+except Exception as e:
+    print(f"Error: {e}")
 
 @app.route('/')
 def home():
@@ -10,16 +30,14 @@ def home():
 @app.route('/send_message', methods=['POST'])
 def send_message():
     user_message = request.json.get('message')
-    
-    # Here, integrate your LLM bot logic
-    # For example, if using a function named `get_bot_response`
     bot_response = get_bot_response(user_message) # Replace with your function
     
     return jsonify({'reply': bot_response})
 
 def get_bot_response(message):
     # Simulated response for demonstration
-    return f"You said: {message} and here is bot response...."  # Replace with your LLM interaction logic
+    response = model.invoke([HumanMessage(content=message)])
+    return response.content  # Replace with your LLM interaction logic
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
